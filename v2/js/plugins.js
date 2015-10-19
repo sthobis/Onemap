3daa31eee973174c67e3c5a39bfbb4d2fb47c8c5
Stefanus Thobi Sinaga <stefanus@xtremax.com>
2015-10-19 17:02:09 +0700

    update v2 using wgs84

--- v2/js/plugins.js
+++ v2/js/plugins.js
@@ -34,9 +34,7 @@ XtrOnemap.UIComponents = function( customSetting ) {
 		// Check if container exist
 		if ($('#map-container').length) {
 			OneMap = new GetOneMap('map-container', 'sm');
-		}
-
-		console.log("Init onemap finished.");
+		} 
 	}
 
 	// method to load shapefile data
@@ -63,54 +61,9 @@ XtrOnemap.UIComponents = function( customSetting ) {
 			//OneMap.overlayKML('data/dengue.kml');
 
 			// using OneMap mashup/theme data API
-			//overlayTheme("DENGUE_CLUSTER");
-
-			console.log('========================================\nDrawing Overlay\n===========================================');
-			// using shapefile
-			drawShapefile("Dengue_Cluster");
-			console.log('========================================\nOverlay Drawn\n===========================================');
+			overlayTheme("DENGUE_CLUSTER");
 		} else {
 			setTimeout(drawOverlay, 100);
-		}
-	}
-
-	var drawShapefile = function(inputTheme) {
-
-		// init theme
-		var themeName = inputTheme;
-		mashup = new MashupData();
-		mashup.themeName = themeName;
-		mashup.extent = OneMap.map.extent.xmin + "," + OneMap.map.extent.ymin + "," + OneMap.map.extent.xmax + "," + OneMap.map.extent.ymax;
-
-		themeGraphicsLayer = new esri.layers.GraphicsLayer();
-		themeGraphicsLayer.id = themeName;
-		//mashup.GetMashupData(parseShapefileData);
-		parseShapefileData();
-		OneMap.map.addLayer(themeGraphicsLayer);
-
-		OneMap.map.infoWindow.resize(300, 200);
-		OneMap.map.infoWindow.hide();
-		OneMap.onOneMapExtentChange(overlayThemeOnExtentChange);
-
-		console.log("drawShapefile reached");
-
-		try {
-			//set graphic onclick event
-			dojo.connect(themeGraphicsLayer, "onClick", function (evt) {//debugger
-				mashup.GetDataForCallout(evt.graphic, "", function (results) {//debugger
-
-					// using API's formatter
-					// var formattedResults = mashup.formatResultsEnhanced(results);
-					// using custom formatter
-					var formattedResults = customFormatResultsEnhanced(results);
-					OneMap.map.infoWindow.setTitle(themeName);
-					OneMap.map.infoWindow.setContent(formattedResults);
-					OneMap.map.infoWindow.show(evt.screenPoint, OneMap.map.getInfoWindowAnchor(evt.screenPoint));
-				});
-			})
-		}
-		catch (err) { 
-
 		}
 	}
 
@@ -155,133 +108,7 @@ XtrOnemap.UIComponents = function( customSetting ) {
 
 	var overlayThemeOnExtentChange = function(extent) {
 		mashup.extent = extent.xmin + "," + extent.ymin + "," + extent.xmax + "," + extent.ymax;
-		//mashup.GetMashupData(parseShapefileData)
-		parseShapefileData();
-		themeGraphicsLayer.show();
-	}
-
-	var parseShapefileData = function() {
-		function hexToRgb(hex) {
-			var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-			return result ? {
-				r: parseInt(result[1], 16),
-				g: parseInt(result[2], 16),
-				b: parseInt(result[3], 16)
-			} : null;
-		}
-		var results = shapefileData;
-
-		if (results == "No results") {
-			alert("Theme not fraud. Please check theme name.");
-			return
-		}
-
-		var featcount = shapefileData.features.length;
-		//var iconPath = mashupResults.iconPath
-		// asumming one file only contains one type of shape (point/line/polygon) and contains at least there is one data/features
-		var featType = shapefileData.features[0].geometry.type;
-		themeGraphicsLayer.clear();
-		var i;
-		var xPnt;
-		var yPnt;
-		var xCord;
-		var yCord;
-
-		var symbolColor = '#E600A9';
-
-		var pntArr = new Array();
-
-		if (results.length == 0) {
-			return
-		}
-
-		if (featType == "Point") {
-			//process all the results
-			for (i = 0; i < results.length; i++) {
-
-				//create point graphic on map using generatePointGraphic function
-				var PointGraphic = generatePointGraphic(results[i].XY, results[i].ICON_NAME, iconPath)
-				//set graphic attributes
-				PointGraphic.attributes = results[i]
-				//add newly created graphic in graphiclayer
-				themeGraphicsLayer.add(PointGraphic);
-			}
-		}
-		else if (featType == "Polygon") {
-			var polygon;
-			for (i = 0; i < shapefileData.features.length; i++) {
-				if (shapefileData.features[i].properties.Color != undefined && shapefileData.features[i].properties.Color != "") {
-					var polyColor = shapefileData.features[i].properties.Color;
-					var r = hexToRgb(polyColor).r;
-					var g = hexToRgb(polyColor).g;
-					var b = hexToRgb(polyColor).b;
-				}
-				else {
-					var polyColor = symbolColor;
-					var r = hexToRgb(polyColor).r;
-					var g = hexToRgb(polyColor).g;
-					var b = hexToRgb(polyColor).b;
-				}
-				pntArr = [];
-				polygon = new esri.geometry.Polygon(new esri.SpatialReference({wkid:3414}));
-
-				for (var x = 0; x < shapefileData.features[i].geometry.coordinates[0].length; x++) {
-					xCord = shapefileData.features[i].geometry.coordinates[0][0];
-					yCord = shapefileData.features[i].geometry.coordinates[0][1];
-
-					var PointLocation = new esri.geometry.Point(xCord, yCord, new esri.SpatialReference({ wkid: 3414 }))
-					pntArr.push(PointLocation);
-				}
-				polygon.addRing(pntArr);
-
-				gra = new esri.Graphic;
-				gra.geometry = polygon;
-				gra.attributes = results[i];
-
-				var sfs = new esri.symbol.SimpleFillSymbol(esri.symbol.SimpleFillSymbol.STYLE_SOLID,
-					  new esri.symbol.SimpleLineSymbol(esri.symbol.SimpleLineSymbol.STYLE_SOLID,
-					  new dojo.Color([0, 0, 0]), 2), new dojo.Color([r, g, b, 0.8]));
-
-				gra.symbol = sfs;
-				themeGraphicsLayer.add(gra);
-			}
-		}
-		else if (featType == "Line") {
-			var pLine;
-			for (i = 0; i < results.length; i++) {
-				if (mashupResults.results[i].SYMBOLCOLOR != undefined && mashupResults.results[i].SYMBOLCOLOR != "") {
-					var polyColor = mashupResults.results[i].SYMBOLCOLOR;
-					var r = hexToRgb(polyColor).r;
-					var g = hexToRgb(polyColor).g;
-					var b = hexToRgb(polyColor).b;
-				}
-				else if (mashupResults.results[i].SYMBOLCOLOR == "") {
-					var r = 0;
-					var g = 0;
-					var b = 0;
-				}
-				pntArr = [];
-				pLine = new esri.geometry.Polyline(new esri.SpatialReference({wkid:3414}));
-
-				for (var x = 0; x < results[i].XY.split("|").length; x++) {
-					xCord = results[i].XY.split("|")[x].split(",")[0];
-					yCord = results[i].XY.split("|")[x].split(",")[1];
-
-					var PointLocation = new esri.geometry.Point(xCord, yCord, new esri.SpatialReference({ wkid: 3414 }))
-					pntArr.push(PointLocation);
-				}
-				pLine.addPath(pntArr);
-
-				gra = new esri.Graphic;
-				gra.geometry = pLine;
-				gra.attributes = results[i];
-
-				var sfs = new esri.symbol.SimpleLineSymbol(esri.symbol.SimpleLineSymbol.STYLE_SOLID,
-					  new dojo.Color([r, g, b]), 2);
-				gra.symbol = sfs;
-				themeGraphicsLayer.add(gra);
-			}
-		}
+		mashup.GetMashupData(overlayData);
 	}
 
 	var overlayData = function(mashupResults) {
@@ -293,12 +120,14 @@ XtrOnemap.UIComponents = function( customSetting ) {
 				b: parseInt(result[3], 16)
 			} : null;
 		}
-		var results = mashupResults.results;
+		var results = shapefileData;
 
 		if (results == "No results") {
 			alert("Theme not found. Please check theme name.");
 			return
 		}
+
+		console.log(mashupResults);
 
 		var featcount = mashupResults.count;
 		var iconPath = mashupResults.iconPath
@@ -330,26 +159,31 @@ XtrOnemap.UIComponents = function( customSetting ) {
 		}
 		else if (featType == "Polygon") {
 			var polygon;
-			for (i = 0; i < results.length; i++) {
-				if (mashupResults.results[i].SYMBOLCOLOR != undefined && mashupResults.results[i].SYMBOLCOLOR != "") {
-					var polyColor = mashupResults.results[i].SYMBOLCOLOR;
+			for (i = 0; i < shapefileData.features.length; i++) {
+				if (shapefileData.features[i].properties.Color != undefined && shapefileData.features[i].properties.Color != "") {
+					var polyColor = shapefileData.features[i].properties.Color;
 					var r = hexToRgb(polyColor).r;
 					var g = hexToRgb(polyColor).g;
 					var b = hexToRgb(polyColor).b;
 				}
-				else if (mashupResults.results[i].SYMBOLCOLOR == "") {
-					var r = 0;
-					var g = 0;
-					var b = 0;
+				else {
+					var polyColor = '#E600A9';
+					var r = hexToRgb(polyColor).r;
+					var g = hexToRgb(polyColor).g;
+					var b = hexToRgb(polyColor).b;
 				}
 				pntArr = [];
 				polygon = new esri.geometry.Polygon(new esri.SpatialReference({wkid:3414}));
 
-				for (var x = 0; x < results[i].XY.split("|").length; x++) {
-					xCord = results[i].XY.split("|")[x].split(",")[0];
-					yCord = results[i].XY.split("|")[x].split(",")[1];
+				var baseUrl = 'http://tasks.arcgisonline.com/ArcGIS/rest/services/Geometry/GeometryServer/project?inSR=4326&outSR=3414';
+				var geometries = '';
+				var returnFormat = '&f=pjson';
 
-					var PointLocation = new esri.geometry.Point(xCord, yCord, new esri.SpatialReference({ wkid: 3414 }))
+				for (var x = 0; x < shapefileData.features[i].geometry.coordinates[0].length; x++) {
+					xCord = shapefileData.features[i].geometry.coordinates[0][j][0];
+					yCord = shapefileData.features[i].geometry.coordinates[0][j][1];
+
+					var PointLocation = new esri.geometry.Point(xCord, yCord, new esri.SpatialReference({ wkid: 4326 }))
 					pntArr.push(PointLocation);
 				}
 				polygon.addRing(pntArr);
